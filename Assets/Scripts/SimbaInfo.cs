4e419977ea1e66d45f353539b8330a5d1f59b715
Steven Peregrine <38218791+Speregrine@users.noreply.github.com>
2020-02-19 12:40:59 -0500

    Update SimbaInfo.cs

--- Assets/Scripts/SimbaInfo.cs
+++ Assets/Scripts/SimbaInfo.cs
@@ -5,7 +5,7 @@ using Nethereum.HdWallet;
 public class SimbaInfo : MonoBehaviour
 {
     public static string Url = "https://api.simbachain.com/v1/highscore";
-    public static string ApiKey = "09c5693b8a039565905193aea412b0671ccd115efc2336958de950f23dcdd245";
+    public static string ApiKey = "";
     public static string WalletSeed = "";
     public static Wallet Wallet;
 }
